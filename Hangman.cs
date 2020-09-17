366bd2ba79b90115e5d007573ff19ed000068520
benjaminwsh <benjaminwg1028@gmail.com>
2020-09-17 13:38:22 +0100

    Bruh 4

--- Hangman.cs
+++ Hangman.cs
@@ -2,10 +2,13 @@ namespace DefaultNamespace
 {
     public class Hangman
     {
-        Console.WriteLine("Test");
+        string[] = wordArray = ['Apple', 'Banana', 'Orange']
+        Console.WriteLine();
+        
         //Change 1
         Console.ReadLine("Bruh");
         Console.ReadLine("Bruh2");
         Console.ReadLine("Bruh3");
+        Console.ReadLine("Bruh4")
     }
 }
