9b04606d019ea83900884663cdc8668c6fe56c52
gtfyds <07otaite-jupp@brightoncollege.net>
2020-09-17 13:44:55 +0100

    Tasks

--- Hangman.cs
+++ Hangman.cs
@@ -4,8 +4,9 @@ namespace DefaultNamespace
     {
         string[] = wordArray = ['Apple', 'Banana', 'Orange']
         Console.WriteLine();
-        
-        //Change 1
+        // Ben - enter the words and allow user to choose
+        // Ollie - function that loops through the word to see where letters are
+        // Main while loop
         Console.ReadLine("Bruh");
         Console.ReadLine("Bruh2");
         Console.ReadLine("Bruh3");
